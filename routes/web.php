515e7604997a86d4fda01cf4c7622b9f939faf1a
Maksim <volynretrashetyn@gmail.com>
2023-02-16 23:23:45 +0300

    There were created UserController, two templates, and two routes.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/login', [UserController::class, 'create'])->name('register.create');
+Route::post('/login', [UserController::class, 'store'])->name('register.store');
