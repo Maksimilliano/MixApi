48e2b5c4d74237da71364048644f920178f3bb3d
Davut <sergeevdavut@mail.ru>
2023-02-17 12:24:10 +0300

    Added base layouts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,10 +31,47 @@
         </div>
     </div>
     <div class="container">
-        <div class="info-block">
-            <div class="content">
-                <span class="fw-bold">{{ env('APP_NAME') }}</span>
-                - is a huge collection of detailed information and community on the most popular franchises
+        <div class="section">
+            <h2 class="mt-4 mb-4 text-center fs-1">Let us tell you about project..</h2>
+            <div class="info-block">
+                <div class="content">
+                    <span class="fw-bold">{{ env('APP_NAME') }}</span>
+                    - is a huge collection of detailed information and community on the most popular franchises
+                </div>
+                <div class="preview rounded-circle overflow-hidden">
+                    <img src="{{ asset('/sources/mando-with-grogu.jpg') }}" alt="">
+                </div>
+            </div>
+            <div class="info-block__reverse">
+                <div class="preview rounded-circle overflow-hidden">
+                    <img src="{{ asset('/sources/vader.jpg') }}" alt="">
+                </div>
+                <div class="content">
+                    <span class="fw-bold">{{ env('APP_NAME') }}</span>
+                    - is a huge collection of detailed information and community on the most popular franchises
+                </div>
+            </div>
+            <div class="info-block">
+                <div class="content">
+                    <span class="fw-bold">{{ env('APP_NAME') }}</span>
+                    - is a huge collection of detailed information and community on the most popular franchises
+                </div>
+                <div class="preview rounded-circle overflow-hidden">
+                    <img src="{{ asset('/sources/marvel.jpg') }}" alt="">
+                </div>
+            </div>
+            <div class="info-block__reverse">
+                <div class="preview rounded-circle overflow-hidden">
+                    <img src="{{ asset('/sources/all-heroes.jpg') }}" alt="">
+                </div>
+                <div class="content">
+                    <span class="fw-bold">{{ env('APP_NAME') }}</span>
+                    - is a huge collection of detailed information and community on the most popular franchises
+                </div>
+            </div>
+            <div class="divider-block">
+                <div class="fs-2"><a class="nav-link" href="/login">This is the way</a></div>
+                <img src="{{ asset('/sources/mandalorian.jpg') }}" alt="">
             </div>
         </div>
     </div>
